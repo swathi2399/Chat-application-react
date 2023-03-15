a420c48e4ab1ba668091acb3cf8bd3cc1f23bb00
swathi2399 <48547312+swathi2399@users.noreply.github.com>
2023-03-15 19:05:46 -0400

    This is the first commit that I'm making - SWATHI

---
+++ client/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+//entire react component is gong to be inside App.js - root component
+ReactDOM.render(<App />, document.getElementById('root'));
